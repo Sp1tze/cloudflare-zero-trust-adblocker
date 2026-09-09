62e512a8f735d81038b6360e71c2f72e1faa63c5
Sp1tze <121177193+Sp1tze@users.noreply.github.com>
2026-09-09 19:27:38 +0300

    Update terminate.rb

--- lib/utils/terminate.rb
+++ lib/utils/terminate.rb
@@ -12,7 +12,7 @@ module Services
 
     def self.fetch_existing_lists
       Utils::Log.logger.info('Retrieving existing lists...')
-      result = API::CloudflareAPI.api_call(:get, "https://api.cloudflare.com/client/v4/accounts/#{self.account_id}/gateway/rules/lists")
+      result = API::CloudflareAPI.api_call(:get, "https://api.cloudflare.com/client/v4/accounts/#{account_id}/gateway/rules/lists")
       return [] if result.nil?
 
       Utils::Log.logger.debug("Retrieved #{result.size} existing lists")
@@ -23,12 +23,12 @@ module Services
       Utils::Log.logger.info("Creating list '#{name}'...")
       items = domains.map { |domain| { 'value' => domain } }
       body = { name: name, description: description, type: 'DOMAIN', items: items }
-      API::CloudflareAPI.api_call(:post, "https://api.cloudflare.com/client/v4/accounts/#{self.account_id}/gateway/rules/lists", body)
+      API::CloudflareAPI.api_call(:post, "https://api.cloudflare.com/client/v4/accounts/#{account_id}/gateway/rules/lists", body)
     end
 
     def self.delete_list(list_id)
       Utils::Log.logger.info("Deleting list with ID #{list_id}...")
-      API::CloudflareAPI.api_call(:delete, "https://api.cloudflare.com/client/v4/accounts/#{self.account_id}/gateway/rules/lists/#{list_id}")
+      API::CloudflareAPI.api_call(:delete, "https://api.cloudflare.com/client/v4/accounts/#{account_id}/gateway/rules/lists/#{list_id}")
     end
   end
 end
