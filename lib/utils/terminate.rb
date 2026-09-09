c8d95629ce4c677cbbc4c969ad3c0328def3bde7
Sp1tze <121177193+Sp1tze@users.noreply.github.com>
2026-09-09 19:53:56 +0300

    Update terminate.rb

--- lib/utils/terminate.rb
+++ lib/utils/terminate.rb
@@ -1,34 +1,10 @@
-require_relative '../api/cloudflare_api'
-require_relative '../utils/logger'
-require_relative '../utils/terminate'
+require_relative 'logger'
 
-module Services
-  class ListManager
-    def self.account_id
-      id = ENV.fetch('CF_ACCOUNT_ID', nil)
-      Utils::Terminate.exit_with_error("CRITICAL: CF_ACCOUNT_ID is missing, empty, or has an invalid length! Value: '#{id}'") if id.nil? || id.strip.empty? || id.length != 32
-      id
-    end
-
-    def self.fetch_existing_lists
-      Utils::Log.logger.info('Retrieving existing lists...')
-      result = API::CloudflareAPI.api_call(:get, "https://api.cloudflare.com/client/v4/accounts/#{account_id}/gateway/rules/lists")
-      return [] if result.nil?
-
-      Utils::Log.logger.debug("Retrieved #{result.size} existing lists")
-      result
-    end
-
-    def self.create_list(name, description, domains)
-      Utils::Log.logger.info("Creating list '#{name}'...")
-      items = domains.map { |domain| { 'value' => domain } }
-      body = { name: name, description: description, type: 'DOMAIN', items: items }
-      API::CloudflareAPI.api_call(:post, "https://api.cloudflare.com/client/v4/accounts/#{account_id}/gateway/rules/lists", body)
-    end
-
-    def self.delete_list(list_id)
-      Utils::Log.logger.info("Deleting list with ID #{list_id}...")
-      API::CloudflareAPI.api_call(:delete, "https://api.cloudflare.com/client/v4/accounts/#{account_id}/gateway/rules/lists/#{list_id}")
+module Utils
+  class Terminate
+    def self.exit_with_error(message)
+      ::Utils::Log.logger.error(message)
+      exit(1)
     end
   end
 end
