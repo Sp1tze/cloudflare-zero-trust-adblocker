abc079ec59955084db1fa3d31df569f4df40984f
Sp1tze <121177193+Sp1tze@users.noreply.github.com>
2026-09-09 20:02:36 +0300

    Update list_manager.rb

--- lib/services/list_manager.rb
+++ lib/services/list_manager.rb
@@ -5,9 +5,10 @@ require_relative '../utils/terminate'
 module Services
   class ListManager
     def self.account_id
-      id = ENV.fetch('CF_ACCOUNT_ID', nil)
-      ::Utils::Terminate.exit_with_error("CRITICAL: CF_ACCOUNT_ID is missing, empty, or has an invalid length! Value: '#{id}'") if id.nil? || id.strip.empty? || id.length != 32
-      id
+      id = ENV['CF_ACCOUNT_ID']
+      ::Utils::Log.logger.info("DEBUG: CF_ACCOUNT_ID value is '#{id}' (length: #{id&.length})")
+      ::Utils::Terminate.exit_with_error("CRITICAL: CF_ACCOUNT_ID is missing or invalid!") if id.nil? || id.strip.empty? || id.length != 32
+      id.strip
     end
 
     def self.fetch_existing_lists
